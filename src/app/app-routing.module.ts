e28f381062a6a233b61e30dfa964f21bc9e22072
Zeemari <Zenntia@gmail.com>
2023-02-26 17:11:17 +0100

    driver section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./header/header.module').then((m) => m.HeaderModule),
   },
+  {
+    path: 'partner',
+    loadChildren: () =>
+      import('./driver/partner/partner.module').then((m) => m.PartnerModule),
+  },
 ];
 
 @NgModule({
